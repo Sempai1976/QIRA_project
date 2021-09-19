5ab2154292bc761e96b38a1d79e5ce995677877b
Vitaliy Lyakishev <vitaliy@supremedia.com>
2021-09-19 09:08:08 +0300

    Clean script.js

--- symfony/public/js/script.js
+++ symfony/public/js/script.js
@@ -1,4 +1,3 @@
 $('#confirm-part-delete').on('show.bs.modal', function(e) {
    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
-  // console.log('test', $(e.relatedTarget).data('href'));
 });
